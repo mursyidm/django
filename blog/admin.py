4df110af98b440e896d82dc53866a0193f8561c9
s53676 <msy5@yahoo.com>
2020-01-15 16:01:30 +0800

    Changed the HTML for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+#from django.contrib import admin
+#from .models import Post
 
 # Register your models here.
+
+#admin.site.register(Post)
+
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
